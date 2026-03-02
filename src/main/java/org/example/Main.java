5c9f0edbf9777a94a751c4403d21281c884a03fa
referty <savinandrej895@gmail.com>
2026-03-02 21:13:42 +0400

    bmi

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gu
+public class Main {
+    public static void main(String[] args) {
+        Scanner inputHandler = new Scanner(System.in);
+        String weight = inputHandler.nextLine();
+        String height = inputHandler.nextLine();
+        double w;
+        double h;
+        double bmi;
+        try {
+            w = Double.parseDouble(weight);
+            h = Double.parseDouble(height) / 100;
+        } catch (NumberFormatException e) {
+            System.out.println("НЕВЕРНО БЛЯТЬ");
+            return;
+        }
+        bmi = Math.round(w * 100 / Math.pow (h,2) ) / 100f;
+        System.out.printf("%.2f\n",bmi);
+        if (bmi < 18.5) {
+            System.out.println("Почти Андей Савин(слегка худее)");
+        }
+        if (bmi < 25 && bmi >=18.5){
+            System.out.println("Поздравляю вы как Андрей Савин");
+        }
+        if (bmi < 30 && bmi >=25){
+            System.out.println("К сожалению, вы пухлее Андрея Савина.");
+        }
+        if (bmi >= 30){
+            System.out.println("Я безумно вам сочувствую, но вы даже не приблизились, а даже очень сильно привысили вес Андрюшки Савина...");
+        }
+    }
+}
+
+
