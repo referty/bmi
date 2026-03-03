2674d0087d95bb5d4c53e51cb6f756faf70d8256
referty <savinandrej895@gmail.com>
2026-03-03 17:28:35 +0400

    косметическое изменение

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
-
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gu
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите сначала ваш вес, после введите ваш рост.");
